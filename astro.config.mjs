ff1d54d595b3b66889a3850297ee63d0e9d481d5
CH <clint@thestaticvoid.net>
2022-02-02 20:59:20 +0100

    Updates

---
+++ astro.config.mjs
@@ -0,0 +1,53 @@
+// Full Astro Configuration API Documentation:
+// https://docs.astro.build/reference/configuration-reference
+import {fileURLToPath} from 'url';
+import {resolve, dirname} from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import {nodeResolve} from '@rollup/plugin-node-resolve';
+import alias from '@rollup/plugin-alias';
+
+
+// @type-check enabled!
+// VSCode and other TypeScript-enabled text editors will provide auto-completion,
+// helpful tooltips, and warnings if your exported object is invalid.
+// You can disable this by removing "@ts-check" and `@type` comments below.
+
+// @ts-check
+export default /** @type {import('astro').AstroUserConfig} */ ({
+  buildOptions: {
+    site: `https://clinth.github.io/ixfx/`
+  },
+  renderers: [
+    // Enable the Preact renderer to support Preact JSX components.
+    `@astrojs/renderer-preact`,
+    // Enable the Preact renderer to support Preact JSX components.
+    // Enable the React renderer, for the Algolia search component
+    //`@astrojs/renderer-react`,
+    `@astrojs/renderer-lit`
+  ],
+  vite: {
+    build: {
+      target: `esnext`
+    },
+    // resolve: {
+    //   alias: {
+    //     '~': () => {
+    //       const base = resolve(dirname(fileURLToPath(import.meta.url)), `../src/`);
+    //       console.log(base);
+    //       return base;
+    //     }
+    //   }
+    // },
+    plugins: [
+      tsconfigPaths({root: `../`}),
+      alias({
+        entries: [
+          {find: `~/`, replacement: resolve(dirname(fileURLToPath(import.meta.url)), `../src/`)}
+        ]
+      }),
+      nodeResolve({
+        extensions: [`.js`, `.ts`]
+      })
+    ]
+  }
+});
---
+++ src/pages/modulation/envelope.ts
@@ -0,0 +1,119 @@
+/* eslint-disable */
+import * as Envelopes from 'ixfx/lib/modulation';
+import {Forms, resolveEl, log} from 'ixfx/lib/dom';
+import {Palette, Plot} from 'ixfx/lib/visual';
+import {fromEvent, debounceTime} from 'rxjs';
+import {map} from 'rxjs/operators';
+
+// Setup data logger
+const envDataLog = log(`#envDataStream`, {
+  minIntervalMs: 20,
+  capacity: 150
+});
+
+// Setup plot
+const palette = Palette.create();
+palette.add(`scaled`, `yellow`);
+palette.add('scaled-axis', 'whitesmoke');
+
+const envData = Plot.plot2(`#envData`, {
+  capacity: 300,
+  showYAxis: true,
+  palette: palette,
+  lineWidth: 3
+});
+
+// Setup envelope
+let opts: Envelopes.AdsrOpts = {
+  ...Envelopes.defaultAdsrOpts(),
+  attackBend: 1,
+  decayBend: -1,
+  releaseBend: 1
+};
+let shouldLoop = false;
+let env = Envelopes.adsr({...opts, shouldLoop});
+
+env.addEventListener(`change`, ev => {
+  Forms.button(`#btnRelease`).disabled = ev.newState !== `sustain`;
+});
+
+Forms.button(`#btnTriggerHold`, () => {
+  envDataLog.clear();
+  env.trigger(true);
+  startDrawing();
+});
+Forms.button(`#btnRelease`, () => {
+  env.release();
+});
+Forms.button(`#btnTrigger`, () => {
+  envDataLog.clear();
+  env.trigger();
+  startDrawing();
+});
+
+const selectShow = Forms.select(`#selectShow`, (val) => {
+  envData.clear();
+  envDataLog.log()
+  startDrawing();
+});
+
+// Update playground if envelope is changed
+fromEvent(resolveEl(`#envEditor`), `change`)
+  .pipe(
+    map((v: CustomEvent) => v.detail as Envelopes.AdsrOpts),
+    debounceTime(1000))
+  .subscribe(v => {
+    opts = v;
+    console.log('hello')
+    updateEnvelope();
+  });
+
+
+const updateEnvelope = () => {
+  try {
+    env = Envelopes.adsr({...opts, shouldLoop});
+    env.trigger();
+    envDataLog.clear();
+    envData.clear();
+    startDrawing();
+  } catch (err) {
+    envDataLog.error(err);
+  }
+}
+
+Forms.checkbox(`#chkLooping`, (newVal) => {
+  shouldLoop = newVal;
+  updateEnvelope();
+});
+
+
+
+let isDrawing = false;
+
+const startDrawing = () => {
+  if (isDrawing) return;
+  const draw = function () {
+    let [stage, scaled, amt] = env.compute();
+    if (stage === undefined) {
+      isDrawing = false;
+      return;
+    }
+
+    // Plot & log data
+    const what = selectShow.value === `raw` ? amt : scaled;
+    envData.add(what);
+    envDataLog.log(`${stage} ${what.toFixed(3)}`);
+
+    if (!env.isDone) {
+      isDrawing = true;
+      window.requestAnimationFrame(draw);
+    } else {
+      console.log(`Envelope done`);
+      isDrawing = false;
+    }
+  }
+  window.requestAnimationFrame(draw);
+}
+
+
+updateEnvelope();
